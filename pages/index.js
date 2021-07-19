e44ad80c77c5a588719bab4a078bc3b8414eb7a5
Rokas <rookas.rudzenskas@gmail.com>
2021-07-19 11:15:39 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -26,8 +26,19 @@ export default function Home() {
                     <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
                         <img loading="lazy" src="https://ssl.gstatic.com/docs/templates/thumbnails/docs-blank-googlecolors.png" />
                     </div>
-                    <p>Blank</p>
+                    <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">Blank</p>
                 </div>
+            </div>
+        </section>
+
+        <section className="bg-white px-10 md:px-0">
+            <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
+                <div className="flex items-center justify-between pb-5">
+                    <h2 className="font-medium flex-grow">My Documents</h2>
+                    <p className="mr-12">Date created</p>
+                    <Icon name="folder" size="3xl" color="gray" />
+                </div>
+
             </div>
         </section>
 
