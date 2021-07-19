f86bf553033a01ca13ff187d6af2d4e457a9f9d5
Rokas <rookas.rudzenskas@gmail.com>
2021-07-19 13:06:17 +0300

    Rokas

--- components/Login.js
+++ components/Login.js
@@ -3,9 +3,14 @@ import Head from "next/head";
 import Icon from "@material-tailwind/react/Icon";
 import Image from "next/Image";
 import {signIn} from "next-auth/client";
+import Button from "@material-tailwind/react/Button";
 
 
 const Login = () => {
+
+    const signIn = () => {
+
+    }
     return (
         <div className="">
             <Head>
@@ -15,7 +20,7 @@ const Login = () => {
 
             <div className="flex flex-col items-center justify-center min-h-screen py-2">
                 <img src="https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Google_Docs.max-1100x1100.png" height="300" width="500" alt=""/>
-                <h2>Hello</h2>
+                <Button className="w-44 mt-10" color="blue" buttonType="filled" ripples="light" onClick={signIn}>Sign In To Docs</Button>
             </div>
         </div>
     );
