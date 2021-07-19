285ff9e0e702eb6bfd58b7fde85371c2d3b62e55
Rokas <rookas.rudzenskas@gmail.com>
2021-07-19 13:54:41 +0300

    Roaks

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import 'tailwindcss/tailwind.css'
 import "@material-tailwind/react/tailwind.css";
 import Head from 'next/head';
+import {Provider} from "next-auth/client";
+
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -10,8 +12,13 @@ function MyApp({ Component, pageProps }) {
               href="https://fonts.googleapis.com/icon?family=Material+Icons"
               rel="stylesheet"
           />
+          <title>Rookas Docs App</title>
+          <link rel="icon" href="https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Google_Docs.max-1100x1100.png" />
       </Head>
-      <Component {...pageProps} />
+        {/* passing all the stuff to to the application*/}
+        <Provider session={pageProps.session}>
+            <Component {...pageProps} />
+        </Provider>
     </>
   )
 }
