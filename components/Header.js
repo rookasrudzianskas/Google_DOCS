164778bb78b8619b16a2930d1b2bfd9d6c0129c4
Rokas <rookas.rudzenskas@gmail.com>
2021-07-19 09:52:17 +0300

    Roaks

--- components/Header.js
+++ components/Header.js
@@ -5,8 +5,11 @@ import Icon from "@material-tailwind/react/Icon";
 const Header = () => {
     return (
         <div className="flex items-center">
-            <Button color="gray" buttonType="outline" rounded={true} iconOnly={true} ripple="dark" className="hidden md:inline-flex h-20 w-20 border-0"/>
+            <Button color="gray" buttonType="outline" rounded={true} iconOnly={true} ripple="dark" className="hidden md:inline-flex h-20 w-20 border-0">
             <Icon name="menu" size="3xl" />
+            </Button>
+            <Icon className="" name="description" size="5xl" color="blue" />
+            <h1 className="hidden md:inline-flex ml-2 text-gray-700 text-2xl">Docs</h1>
         </div>
     );
 };
