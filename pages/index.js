221bb523f05ffec5a9464abfeec265f06209b2e1
Rokas <rookas.rudzenskas@gmail.com>
2021-07-19 09:25:08 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function Home() {
       <footer className="flex items-center justify-center w-full h-24 border-t">
         <a
           className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://byrookas.com"
           target="_blank"
           rel="noopener noreferrer"
         >
