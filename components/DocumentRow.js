ae31f3ff9e8eab398dccd7dbaab6c6639cb8292a
Rokas <rookas.rudzenskas@gmail.com>
2021-07-19 15:07:42 +0300

    Rokas

--- components/DocumentRow.js
+++ components/DocumentRow.js
@@ -4,7 +4,7 @@ import Icon from "@material-tailwind/react/Icon";
 
 const DocumentRow = ({id, fileName, date }) => {
     return (
-        <div>
+        <div className="flex items-center p-2 rounded-lg hover:bg-gray-100 text-gray-700 text-sm cursor-pointer mr-10 ml-10 mb-5">
             <Icon name="article" size="3xl" color="blue" />
             <p className="flex-grow pl-5 w-10 pr-10 truncate">{fileName}</p>
             <p className="pr-5 text-sm">{date?.toDate().toLocaleDateString()}</p>
