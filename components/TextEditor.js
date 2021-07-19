fd5b9c6cdaed4b40496b64a9a1eb15c7df13a024
Rokas <rookas.rudzenskas@gmail.com>
2021-07-19 16:49:42 +0300

    Rokas

--- components/TextEditor.js
+++ components/TextEditor.js
@@ -1,41 +1,67 @@
-import React, {useState} from 'react';
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import dynamic from "next/dynamic";
-import {EditorState} from "draft-js";
+import { useEffect, useState } from "react";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { EditorState } from "draft-js";
+import { useRouter } from "next/dist/client/router";
+import { convertFromRaw, convertToRaw } from "draft-js";
+import { useSession } from "next-auth/client";
+import { useDocumentOnce } from "react-firebase-hooks/firestore";
 import db from "../firebase";
-import {session, useSession} from "next-auth/client";
-import {useRouter} from "next/dist/client/router";
-import {convertFromRaw, convertToRaw} from "draft-js";
 
-// importing editor in here
-const Editor = dynamic(() => import('react-draft-wysiwyg').then((module) => module.Editor), {
-    ssr: false,
-})
+const Editor = dynamic(
+    () => import("react-draft-wysiwyg").then((module) => module.Editor),
+    {
+        ssr: false,
+    }
+);
 
-
-
-const TextEditor = () => {
-
-    const [editorState, setEditorState] = useState(EditorState.createEmpty());
+function TextEditor() {
     const [session] = useSession();
+    const [editorState, setEditorState] = useState(EditorState.createEmpty());
     const router = useRouter();
-    const {id} = router.query;
+    const { id } = router.query;
+
+    const [snapshot] = useDocumentOnce(
+        db.collection("userDocs").doc(session.user.email).collection("docs").doc(id)
+    );
+
+    useEffect(() => {
+        if (snapshot?.data()?.editorState) {
+            setEditorState(
+                EditorState.createWithContent(
+                    convertFromRaw(snapshot?.data()?.editorState)
+                )
+            );
+        }
+    }, [snapshot]);
 
     const onEditorStateChange = (editorState) => {
-      setEditorState(editorState);
+        setEditorState(editorState);
 
-      db.collection('userDocs').doc(session.user.email).collection('docs').doc(id).set({
-          editorState: convertToRaw(editorState.getCurrentContent()),
-      }, {merge: true});
+        db.collection("userDocs")
+            .doc(session.user.email)
+            .collection("docs")
+            .doc(id)
+            .set(
+                {
+                    editorState: convertToRaw(editorState.getCurrentContent()),
+                },
+                {
+                    merge: true,
+                }
+            );
     };
 
-    // console.log(editorState);
-
     return (
-        <div className="bg-[#f8f9FA] min-h-screen pb-16">
-            <Editor onEditorStateChange={onEditorStateChange} toolbarClassName="flex sticky top-0 z-50 !justify-center mx-auto" editorClassName="mt-6 bg-white shadow-lg max-w-5xl mx-auto mb-12 border p-10" />
+        <div className="bg-[#F8F9FA] min-h-screen pb-16">
+            <Editor
+                editorState={editorState}
+                onEditorStateChange={onEditorStateChange}
+                toolbarClassName="flex sticky top-0 z-50 !justify-center mx-auto"
+                editorClassName="mt-6 p-10 bg-white shadow-lg max-w-5xl mx-auto mb-12 border"
+            />
         </div>
     );
-};
+}
 
 export default TextEditor;
