898e1a0d0f95b67ae8d8cc061231868aff1962a8
Rokas <rookas.rudzenskas@gmail.com>
2021-07-19 16:32:42 +0300

    Rokas

--- components/TextEditor.js
+++ components/TextEditor.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import dynamic from "next/dynamic";
 
@@ -8,7 +8,10 @@ const Editor = dynamic(() => import('react-draft-wysiwyg').then((module) => modu
 })
 
 
+
 const TextEditor = () => {
+
+    const [editorState, setEditorState] = useState('')
     return (
         <div className="bg-[#f8f9FA] min-h-screen pb-16">
             <Editor toolbarClassName="flex sticky top-0 z-50 !justify-center mx-auto" editorClassName="mt-6 bg-white shadow-lg max-w-5xl mx-auto mb-12 border p-10" />
