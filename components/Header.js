c9e284e7dc021df908c81ea17905e131fc10a7d9
Rokas <rookas.rudzenskas@gmail.com>
2021-07-19 09:57:32 +0300

    Roaks

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Icon from "@material-tailwind/react/Icon";
 
 const Header = () => {
     return (
-        <div className=" sticky top-0 z-50 flex items-center">
+        <div className=" sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white">
             <Button color="gray" buttonType="outline" rounded={true} iconOnly={true} ripple="dark" className="h-20 w-20 border-0">
             <Icon name="menu" color="blue" size="3xl" />
             </Button>
