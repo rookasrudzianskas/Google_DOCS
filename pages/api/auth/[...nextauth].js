a2837cf64ea550565b8405f08b65726d83f02642
Rokas <rookas.rudzenskas@gmail.com>
2021-07-19 12:16:01 +0300

    Rokas

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,20 @@
+import NextAuth from 'next-auth';
+import { FirebaseAdapter } from "@next-auth/firebase-adapter";
+import Providers from 'next-auth/providers';
+import db from "../../../firebase";
+
+export default NextAuth({
+    // Configure one or more authentication providers
+    providers: [
+        Providers.Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        // ...add more providers here
+    ],
+
+    adapter: FirebaseAdapter(db),
+
+    // A database is optional, but required to persist accounts in a database
+    database: process.env.DATABASE_URL,
+})
