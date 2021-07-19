ced043b04a85a56c60071512d8151133dec37079
Rokas <rookas.rudzenskas@gmail.com>
2021-07-19 15:12:36 +0300

    Roaks

--- components/DocumentRow.js
+++ components/DocumentRow.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {useRouter} from "next/dist/client/router";
 import Icon from "@material-tailwind/react/Icon";
+import Button from "@material-tailwind/react/Button";
 
 const DocumentRow = ({id, fileName, date }) => {
+
+    const router = useRouter();
     return (
-        <div className="flex items-center p-2 rounded-lg hover:bg-gray-100 text-gray-700 text-sm cursor-pointer mr-10 ml-10 mb-5">
+        <div onClick={() => router.push(`/doc/${id}`)} className="flex items-center p-4 rounded-lg hover:bg-gray-100 text-gray-700 text-sm cursor-pointer">
             <Icon name="article" size="3xl" color="blue" />
             <p className="flex-grow pl-5 w-10 pr-10 truncate">{fileName}</p>
             <p className="pr-5 text-sm">{date?.toDate().toLocaleDateString()}</p>
+
+            <Button color="gray" buttonType="outline" rounded={true} iconOnly={true} ripple="dark" className="border-0" >
+                <Icon name="more_vert" size="3xl" />
+            </Button>
         </div>
     );
 };
