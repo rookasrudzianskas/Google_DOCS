c79352e42b3a524de0303872073a89d12c10e6b8
Rokas <rookas.rudzenskas@gmail.com>
2021-07-19 10:32:03 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Header from "../components/Header";
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
 
 export default function Home() {
   return (
@@ -13,8 +15,11 @@ export default function Home() {
 
         <section className="bg-[#F8F9FA] pb-10 px-10">
             <div className="max-w-3xl mx-auto">
-                <div className="py-6">
-                    <h2>Start a new document</h2>
+                <div className="flex items-center justify-between py-6">
+                    <h2 className="text-gray-700 text-lg">Start a new document</h2>
+                    <Button color="gray" buttonType="outline" iconOnly={true} ripple="dark" className="border-0">
+                        <Icon name="more_vert" size="3xl" />
+                    </Button>
                 </div>
             </div>
         </section>
